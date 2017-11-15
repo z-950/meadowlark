038ff4a32921cbe0ac473d54c726e7f46929ddab
950 <1033766950@qq.com>
2017-11-15 18:19:19 +0800

    单元测试、逻辑测试

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-var fortune = require('./lib/fortune.js');
+const fortune = require('./lib/fortune.js');
 // 设置 handlebars 视图引擎
 const handlebars = require('express-handlebars')
     .create({ defaultLayout:'main' });
@@ -11,12 +11,27 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 // 设置端口
 app.set('port', process.env.PORT || 3000);
-//路由
+// 测试
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' &&
+        req.query.test === '1';
+    next();
+});
+// 路由
 app.get('/', function(req, res) {
     res.render('home');
 });
 app.get('/about', function(req, res){
-    res.render('about', { fortune: fortune.getFortune() } );
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    });
+});
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
 });
 // 404 catch-all 处理器（中间件）
 app.use(function(req, res, next){
@@ -31,6 +46,5 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-    console.log( 'Express started on http://localhost:' +
-    app.get('port') + '; press Ctrl-C to terminate.' );
+    console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
 });
