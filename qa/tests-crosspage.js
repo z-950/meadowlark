38577640c45d97b0136a5f5f5730533ada369a82
950 <1033766950@qq.com>
2017-11-15 22:41:54 +0800

    跨页测试（获取request中的referer）

--- qa/tests-crosspage.js
+++ qa/tests-crosspage.js
@@ -5,29 +5,33 @@ suite('Cross-Page Tests', function(){
     setup(function(){
         browser = new Browser();
     });
+    // 避免超时
+    this.timeout(10000);
     test('requesting a group rate quote from the hood river tour pageshould populate the referrer field',function(done){
-        var referrer = 'http://localhost:3000/tours/hood-river';
-        browser.visit(referrer, function(){
+        var referer = 'http://localhost:3000/tours/hood-river';
+        browser.visit(referer, function(){
             browser.click('.requestGroupRate', function(){
-                assert(browser.field('referrer').value === referrer);
+                // browser.resources['0'].request.headers._headers[0][1]获取referer
+                assert(browser.resources['0'].request.headers._headers[0][1] === referer);
                 done();
             });
         });
     });
     // 页面未完成
-    // test('requesting a group rate from the oregon coast tour page should populate the referrer field',function(done){
-    //     var referrer = 'http://localhost:3000/tours/oregon-coast';
-    //     browser.visit(referrer, function(){
-    //         browser.click('.requestGroupRate', function(){
-    //             assert(browser.field('referrer').value === referrer);
-    //             done();
-    //         });
-    //     });
-    // });
+    test('requesting a group rate from the oregon coast tour page should populate the referrer field',function(done){
+        var referer = 'http://localhost:3000/tours/oregon-coast';
+        browser.visit(referer, function(){
+            browser.click('.requestGroupRate', function(){
+                assert(browser.resources['0'].request.headers._headers[0][1] === referer);
+                done();
+            });
+        });
+    });
     test('visiting the "request group rate" page dirctly should result in an empty referrer field', function(done){
         browser.visit('http://localhost:3000/tours/request-group-rate',
             function(){
-                assert(browser.field('referrer').value === '');
+                // browser.resources['0'].request.headers._headers[0][0]无referer时是accept
+                assert(browser.resources['0'].request.headers._headers[0][0] !== 'referer');
                 done();
         });
     });
