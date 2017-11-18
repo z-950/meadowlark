e2d46ed05515ea4617c86913a75d8a190a1bb230
950 <1033766950@qq.com>
2017-11-18 15:21:59 +0800

    9.6cookie和会话（flash）

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const fortune = require('./lib/fortune.js');
+// 复合表单处理（文件上传）
 const formidable = require('formidable');
+// 密钥
+const credentials = require('./credentials.js');
+// 文件数据
+const fortune = require('./lib/fortune.js');
 // 设置 handlebars 视图引擎
 const handlebars = require('express-handlebars').create({
     defaultLayout:'main',
@@ -15,18 +19,33 @@ const handlebars = require('express-handlebars').create({
 });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+// 设置端口
+app.set('port', process.env.PORT || 3000);
 // 禁止返回头中的powered-by
 app.disable('x-powered-by');
 // 设置静态地址
 app.use(express.static(__dirname + '/public'));
 // 添加中间件
 app.use(require('body-parser')());
-// 设置端口
-app.set('port', process.env.PORT || 3000);
-// 测试
+    // 添加cookie相关(内存存储)
+app.use(require('cookie-parser')(credentials.cookieSecret));
+    // cookie相关？配置内容？
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: credentials.cookieSecret,
+}));
+    // 测试
 app.use(function(req, res, next){
     res.locals.showTests = app.get('env') !== 'production' &&
         req.query.test === '1';
+    next();
+});
+    // flash消息
+app.use(function(req, res, next){
+    // 如果有即显消息， 把它传到上下文中， 然后清除它
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
     next();
 });
 // 路由
@@ -39,22 +58,59 @@ app.get('/about', function(req, res){
         pageTestScript: '/qa/tests-about.js'
     });
 });
-    // 处理post
-app.get('/newsletter', function(req, res){
-    // 我们会在后面学到 CSRF……目前， 只提供一个虚拟值
-    res.render('newsletter', { csrf: 'CSRF token goes here' });
-});
-app.post('/process', function(req, res){
-    if(req.xhr || req.accepts('json,html')==='json'){
-        // 如果发生错误， 应该发送 { error: 'error description' }
-        res.send({ success: true });
-    } else {
-        // 如果发生错误， 应该重定向到错误页面
-        res.redirect(303, '/thank-you');
-    }
-});
 app.get('/thank-you', function(req,res){
     res.render('thank-you');
+});
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
+app.get('/newsletter', function(req, res){
+    res.render('newsletter');
+});
+// for now, we're mocking NewsletterSignup:
+function NewsletterSignup(){
+}
+NewsletterSignup.prototype.save = function(cb){
+	cb();
+};
+// 邮箱正则验证
+const VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
+app.post('/newsletter', function(req, res){
+    var name = req.body.name || '', email = req.body.email || '';
+    // 输入验证
+    if(!email.match(VALID_EMAIL_REGEX)) {
+        if(req.xhr) return res.json({ error: 'Invalid name email address.' });
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Validation error!',
+            message: 'The email address you entered was not valid.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    }
+    new NewsletterSignup({ name: name, email: email }).save(function(err){
+        if(err) {
+            if(req.xhr) return res.json({ error: 'Database error.' });
+            req.session.flash = {
+                type: 'danger',
+                intro: 'Database error!',
+                message: 'There was a database error; please try again later.',
+            }
+            return res.redirect(303, '/newsletter/archive');
+        }
+        if(req.xhr) return res.json({ success: true });
+        req.session.flash = {
+            type: 'success',
+            intro: 'Thank you!',
+            message: 'You have now been signed up for the newsletter.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    });
+});
+app.get('/newsletter/archive', function(req, res){
+	res.render('newsletter/archive');
 });
     // 文件上传
 app.get('/contest/vacation-photo',function(req,res){
@@ -74,13 +130,6 @@ app.post('/contest/vacation-photo/:year/:month', function(req, res){
         console.log(files);
         res.redirect(303, '/thank-you');
     });
-});
-    // 其他
-app.get('/tours/hood-river', function(req, res){
-    res.render('tours/hood-river');
-});
-app.get('/tours/request-group-rate', function(req, res){
-    res.render('tours/request-group-rate');
 });
 // 404 catch-all 处理器（中间件）
 app.use(function(req, res, next){
