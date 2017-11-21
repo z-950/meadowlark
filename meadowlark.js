ee0ef8d57ae17bbb7479154dc3d523dbdeb02c15
950 <1033766950@qq.com>
2017-11-21 09:09:06 +0800

    第12章（压力测试，无网站监控）

--- meadowlark.js
+++ meadowlark.js
@@ -28,6 +28,43 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 // 禁止返回头中的powered-by
 app.disable('x-powered-by');
+app.use(function(req, res, next){
+    // 为这个请求创建一个域
+    var domain = require('domain').create();
+    // 处理这个域中的错误
+    domain.on('error', function(err) {
+        console.error('DOMAIN ERROR CAUGHT\n', err.stack);
+        try {
+            // 在 5 秒内进行故障保护关机
+            setTimeout(function(){
+                console.error('Failsafe shutdown.');
+                process.exit(1);
+            }, 5000);
+            // 从集群中断开
+            var worker = require('cluster').worker;
+            if(worker) worker.disconnect();
+            // 停止接收新请求
+            server.close();
+            try {
+                // 尝试使用 Express 错误路由
+                next(err);
+            } catch(err) {
+                // 如果 Express 错误路由失效， 尝试返回普通文本响应
+                console.error('Express error mechanism failed.\n', err.stack);
+                res.statusCode = 500;
+                res.setHeader('content-type', 'text/plain');
+                res.end('Server error.');
+            }
+        } catch(err){
+            console.error('Unable to send 500 response.\n', err.stack);
+        }
+    });
+    // 向域中添加请求和响应对象
+    domain.add(req);
+    domain.add(res);
+    // 执行该域中剩余的请求链
+    domain.run(next);
+})
 // 设置静态地址
 app.use(express.static(__dirname + '/public'));
 // 添加中间件
@@ -94,6 +131,16 @@ app.get('/tours/oregon-coast', function(req, res){
 });
 app.get('/tours/request-group-rate', function(req, res){
     res.render('tours/request-group-rate');
+});
+    // 非未捕获异常
+app.get('/fail', function(req, res){
+    throw new Error('Nope!');
+});
+    // 未捕获异常
+app.get('/epic-fail', function(req, res){
+    process.nextTick(function(){
+        throw new Error('Kaboom!');
+    });
 });
 app.get('/newsletter', function(req, res){
     res.render('newsletter');
